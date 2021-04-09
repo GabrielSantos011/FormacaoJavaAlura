fe3ceb009a23db4ccc63aa775b64c80cabf4b364
GabrielSantos011 <gabrielrafael.grs@gmail.com>
2021-04-09 02:53:16 -0300

    Ajustes

--- polimorfismo-heranca/src/Funcionario.java
+++ polimorfismo-heranca/src/Funcionario.java
@@ -17,7 +17,9 @@ public abstract class Funcionario { // classe mãe, classe base, super classe
 	 * passa uma sensação de incompleto, tal que a classe filha só não seria obrigada a implementar
 	 * caso implementasse o abstract também
 	 * para manter o exemplo de sobreescrita não irei alterar mas fica essa anotação além de que terei exemplos de interfaces
-	 * vale lembrar que ao colocar um método como abstrato a classe se torna abstrata*/
+	 * vale lembrar que ao colocar um método como abstrato a classe se torna abstrata
+	 * caso fosse abstrato seria:
+	 * public abstract double getBonificacao();*/
 	public double getBonificacao() {// apesar do nome do método ser get
 									// não se trata de um atributo, algo comum tb
 		System.out.println("método de bonificacao do funcionario");
