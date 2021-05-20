a81585dbd223d289de20baf98a015b4c3959762d
GabrielSantos011 <gabrielrafael.grs@gmail.com>
2021-05-20 16:34:32 -0300

    Arrays genéricos - casting

---
+++ Arrays/src/br/com/alura/aula/TesteArrayGenerico.java
@@ -0,0 +1,26 @@
+package br.com.alura.aula;
+
+import br.com.alura.aula.arraysGenericos.Cliente;
+import br.com.alura.aula.arraysGenericos.Conta;
+import br.com.alura.aula.arraysGenericos.ContaCorrente;
+import br.com.alura.aula.arraysGenericos.ContaPoupanca;
+
+public class TesteArrayGenerico {
+	
+	public static void main(String args[]) {
+		
+		Object arrayGenerico [] = new Object[5];
+		
+		arrayGenerico[0] = new ContaCorrente();
+		
+		ContaCorrente cc = (ContaCorrente) arrayGenerico[0]; //toda conta corrente é object portanto este cast é possível
+		ContaPoupanca cp = (ContaPoupanca) arrayGenerico[0]; // isto compila mas dara um erro de cast - eu falei que era uma cp mas na vdd é uma cc
+		
+		Conta arrayConta [] = new Conta[5];
+		
+		Cliente cliente = new Cliente();
+		
+		//arrayConta[0] = (Conta) cliente;~- este é um cast impossível
+	}
+
+}
---
+++ Arrays/src/br/com/alura/aula/arraysGenericos/ContaPoupanca.java
@@ -0,0 +1,5 @@
+package br.com.alura.aula.arraysGenericos;
+
+public class ContaPoupanca extends Conta {
+
+}
