512de1c0ae73e21a97dad299f63a3fc153ab3902
GabrielSantos011 <gabrielrafael.grs@gmail.com>
2021-06-14 16:01:22 -0300

    formatação de strings

--- Programe-com- classe-Object-e-String/src/br/com/alura/aula/string/TesteString.java
+++ Programe-com- classe-Object-e-String/src/br/com/alura/aula/string/TesteString.java
@@ -1,5 +1,9 @@
 package br.com.alura.aula.string;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Locale;
+
 public class TesteString {
 
 	public static void main(String args[]) {
@@ -19,6 +23,7 @@ public class TesteString {
 		nome.toLowerCase();
 		
 		System.out.println(nome);// a saída permanece Gabriel e não gabriel
+		System.out.println("-------------------------------------------------------------");
 		//pois a string é imutavel, seus métodos retornam outra string porém não alteram o seu valor!
 		
 		//Alguns métodos mais utilizados -- exeistem diversos outros, olhas a documentação é o ideal
@@ -39,11 +44,38 @@ public class TesteString {
 		for (int i=0; i < 4; i++) {
 			System.out.println(teste[i]);
 		}
-		
+		System.out.println("---------------------------------------------------------------");
 		/*
 		 * Uma das grandes vantagens da string é ser imutavel, porém uma de suas maiores desvantagens também é essa;
 		 * imagine que precisamos concatenar varias strings, em termo de desempenho seria muito ruim,
 		 * por isso temos o StringBuilder, um tipo que tb implementa CharSequence porém não é imutavel
 		 */
+		
+		//formatação no sysout através de string
+		int valor1 = 5;
+		int valor2 = 5;
+		int valor3 = 5;
+		double valor4 = 10.0;
+		double valor5 = 10.0;
+		
+		//new locale para seguir as formatações do brasil (, ao invés do .)
+		String stringFormatada = String.format(new Locale("pt", "BR"), 
+				"%s estes são os valores -> %04d - %01d - %03d - %010.3f - %.2f", "Olá", valor1, valor2, valor3, valor4, valor5);
+		System.out.println(stringFormatada);
+		//para demonstrar quebra de linha automatica
+		System.out.println(stringFormatada);
+		
+		//isso equivale ao mesmo que - porem neste novvo caso não há quebra de linha automatica
+		//portanto devemos colocar uma quebra de linha que no format é %n
+		
+		System.out.format(new Locale("pt", "BR"), 
+				"%s estes são os valores -> %04d - %01d - %03d - %010.3f - %.2f %n", "Olá", valor1, valor2, valor3, valor4, valor5);
+		System.out.format(new Locale("pt", "BR"), 
+				"%s estes são os valores -> %04d - %01d - %03d - %010.3f - %.2f %n", "Olá", valor1, valor2, valor3, valor4, valor5);
+		
+		System.out.println("-----------------------------------------------------------------------------------");
+		//obs: outra forma para formatar valores
+		System.out.println(new DecimalFormat("0000000000").format(new BigDecimal("1110111")));
+		
 	}
 }
