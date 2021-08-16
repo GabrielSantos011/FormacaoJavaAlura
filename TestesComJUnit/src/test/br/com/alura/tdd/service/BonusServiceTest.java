8bbba77339c51b1115f7c472659f1671af3c7088
GabrielSantos011 <gabrielrafael.grs@gmail.com>
2021-08-16 16:03:55 -0300

    testes para métodos que jogam exceções

--- TestesComJUnit/src/test/br/com/alura/tdd/service/BonusServiceTest.java
+++ TestesComJUnit/src/test/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,5 +1,7 @@
 package test.br.com.alura.tdd.service;
 
+import static org.junit.Assert.fail;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -14,9 +16,25 @@ public class BonusServiceTest {
 	@Test
 	public void calcularBonusParaSalarioMaiorQueDezMil() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Gabriel", LocalDate.now(), new BigDecimal("12000")));
 		
-		Assert.assertEquals(new BigDecimal("0.00"), bonus);
+		//primeira forma de testar métodos que retornam exception
+		//passamos o .class da exception esperada e um lambda do médtodo que joga a exception
+		
+		//assertThrows(IllegalArgumentException.class, 
+		//() -> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
+	
+		//segunda forma, colocamos um try{}catch{}
+		//chamamos o método e o esperado é a exception e portanto caia no catch
+		//caso ele não vá para o catch jogamos um fail pois o comportamento está errado
+		//no catch podemos deixar vazio ou se tivermos validações podemos colocar
+		//neste caso estamos validando a mensagem do erro
+		
+		try {
+			service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+			fail("nao deu exception");
+		} catch (IllegalArgumentException e) {
+			Assert.assertEquals("Funcionario com salario maior do que R$1000 nao pode receber bonus!", e.getMessage());
+		}
 	}
 	
 	@Test
